db50d680d046907dcce84750e589a6176c827d42
Attila Nagy <attila.nagy880@gmail.com>
2018-04-09 12:50:02 +0200

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react';
+import React from 'react-dom';
+import App from './firstComponenet.jsx';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
---
+++ src/todoitem.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class TodoItem extends React.Component {
+  render () {
+    return <li>{this.props.label}</li >;
+  }
+}
+export default TodoItem;
